80550cc1cf4f9a2f07234528c431439cadb9770f
firesong1337 <65440960+firesong1337@users.noreply.github.com>
2022-04-22 09:30:18 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
+﻿// main.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
 #include <iostream>
 #include "BinaryTree.h"
 using namespace std;
 int main() {
-    setlocale(LC_ALL, "Russian");
     BinaryTree MyTree; // создание дерева
     int element;
     // корнем является тот элемент, который ты ввел 1-м
@@ -10,26 +12,28 @@ int main() {
         cin >> element;
         MyTree.Insert(element);
     }
-    cout <<"Дерево: " << endl << MyTree << endl;
+    cout << "Бинарное дерево: " << endl << MyTree << endl;
     for (int i = 0; i < MyTree.Size(); ++i) {
-        if (MyTree.Find(i)){
-            cout << "Элемент " << i << " находится на дереве" << endl;
+        if (MyTree.Find(i)) {
+            cout << "Элемент " << i << " находится на дереве!" << endl;
         }
     }
-    if (MyTree.Size() == 6){ // В данном случае, если в дереве все 10 элементов разные, то он зайдет
-        cout << "Тест 2 пройден" << endl;
+    if (MyTree.Size() == 6) { // В данном случае, если в дереве все 10 элементов разные, то он зайдет
+        cout << "Второй тест выполнен" << endl;
     }
     cout << "Введите элемент для удаления: ";
     cin >> element;
     MyTree.Erase(element); // удаляешь тот элемент, который является корнем, который вводил первым
     cout << MyTree << endl;
-    if (MyTree.Size() == 5){
-        cout << "Тест 4 пройден" << endl;
+    if (MyTree.Size() == 5) {
+        cout << "Четвертый тест выполнен" << endl;
     }
     MyTree.Clear();
-    if (MyTree.Size() == 0){
-        cout << "TТест 5 пройден" << endl;
+    if (MyTree.Size() == 0) {
+        cout << "Пятый тест выполнен" << endl;
     }
     cout << MyTree << endl;
     return 0;
 }
+
+
